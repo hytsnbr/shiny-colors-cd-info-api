2cbf2f9bdac6850400e12055b4106603ec4a8e0d
hytsnbr10519 <haya524mwam@gmail.com>
2023-10-28 08:39:53 +0000

    refactor: delete top url

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,12 +1,8 @@
 import { apiController } from "@/controllers/api_controller.ts";
-import { Router, RouterContext } from "@/deps.ts";
+import { Router } from "@/deps.ts";
 
 const router = new Router();
 
-router.get("/", (ctx: RouterContext<string>) => {
-  ctx.response.headers.set("content-type", "application/json");
-  ctx.response.body = new Object("TOP PAGE");
-});
 router.get("/list", apiController.getList);
 
 export { router };
