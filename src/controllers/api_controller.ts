d906073076f89d5cd8e9203ca7668c8604ba0a0e
hytsnbr10519 <haya524mwam@gmail.com>
2024-05-10 22:28:15 +0900

    refactor: oak v16.0.0のバージョンアップによる変更に対応した

--- src/controllers/api_controller.ts
+++ src/controllers/api_controller.ts
@@ -1,4 +1,4 @@
-import { helpers, RouterContext } from "oak";
+import { RouterContext } from "oak";
 import dataJsonConvertor from "@/functions/data_json_convertor.ts";
 import { CdInfo } from "@/model/cd_info.ts";
 import { CdInfoList } from "@/model/cd_info_list.ts";
@@ -6,17 +6,18 @@ import { Response } from "@/model/response.ts";
 
 export const apiController = {
   getList(ctx: RouterContext<string>) {
-    const {
-      releaseDateStart,
-      releaseDateEnd,
-      recordNumber,
-      recordNumbers,
-      limited,
-      title,
-      series,
-      storeName,
-      isHiRes,
-    } = helpers.getQuery(ctx, { mergeParams: true });
+    const searchParams = ctx.request.url.searchParams;
+    const releaseDateStart = searchParams.get(
+      "releaseDateStart",
+    ) || "";
+    const releaseDateEnd = searchParams.get("releaseDateEnd") || "";
+    const recordNumber = searchParams.get("recordNumber") || "";
+    const recordNumbers = searchParams.get("recordNumbers") || "";
+    const limited = searchParams.get("limited") || "";
+    const title = searchParams.get("title") || "";
+    const series = searchParams.get("series") || "";
+    const storeName = searchParams.get("storeName") || "";
+    const isHiRes = searchParams.get("isHiRes") || "";
 
     const cdInfoList: CdInfoList = dataJsonConvertor();
     const result: CdInfo[] = cdInfoList
